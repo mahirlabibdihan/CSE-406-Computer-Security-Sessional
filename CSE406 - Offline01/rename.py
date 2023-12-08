45f5dd1e6dbc4936e0cedff236331e0b523913b1
Mahir Labib Dihan <mahirlabibdihan@gmail.com>
2023-12-08 14:45:37 +0600

    done

--- CSE406 - Offline01/rename.py
+++ CSE406 - Offline01/rename.py
@@ -3,10 +3,15 @@ import os, fnmatch
 def renameFiles(directory, prefix, filePattern):
     for path, dirs, files in os.walk(os.path.abspath(directory)):
         for filename in fnmatch.filter(files, filePattern):
-            os.rename(os.path.join(path, filename), os.path.join(path, prefix + filename))
+            if prefix == filename[0:len(prefix)]:
+                os.rename(os.path.join(path, filename),
+                          os.path.join(path, filename[len(prefix):]))
+            else:
+                os.rename(os.path.join(path, filename),
+                          os.path.join(path, prefix + filename))
 
 def renameFile(directory, oldfile, newfile):
     os.rename(os.path.join(directory, oldfile), os.path.join(directory, newfile))
             
                 
-renameFiles("upload/1905072/", "1905072_", "*.py");
+renameFiles("./", "1905072_", "*.py")
--- CSE406 - Offline01/submit.sh
+++ CSE406 - Offline01/submit.sh
@@ -1,7 +1,9 @@
 rm upload -r >/dev/null 2>&1
 mkdir upload/1905072 -p
 cp workspace/*.py upload/1905072
+cp rename.py upload/1905072
+cd upload/1905072/
 python rename.py
-cd upload
+cd ..
 zip 1905072.zip 1905072/*
 echo "Files uploaded at upload/1905072.zip"
